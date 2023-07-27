a7fed8d9535c277526d4b812f921c861e689d311
Jair Gomez <jairherber505@gmail.com>
2023-07-27 12:29:08 -0700

    added another model

---
+++ models/Thought.js
@@ -0,0 +1,32 @@
+const { Schema, model } = require("mongoose");
+const Schema = mongoose.Schema;
+
+const thoughtSchema = new Schema({
+  thoughtText: {
+    type: String,
+    required: true,
+    minlength: 1,
+    max_length: 280,
+  },
+  createdAt: {
+    type: Date,
+    default: Date.now,
+    // * Use a getter method to format the timestamp on query
+  },
+  username: {
+    type: String,
+      required: true,
+    // * or more like type: Schema.types.ObjectId?
+  },
+  //? Need help
+  reactions: {},
+  toJSON: {
+    virtuals: true,
+  },
+});
+ //? Is this correct?
+postSchema.virtual("reactionCount").get(function () {
+  return this.reactions.toString();
+});
+
+module.exports = mongoose.model("thought", thoughtSchema);
