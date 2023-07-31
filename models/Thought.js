6d3d077a97d7220e1d1dd88c3c75aa900d9e6347
Jair Gomez <jairherber505@gmail.com>
2023-07-31 11:54:34 -0700

    debugging

--- models/Thought.js
+++ models/Thought.js
@@ -1,29 +1,33 @@
 const { Schema, model } = require("mongoose");
 const reactionSchema = require("./Reaction");
 
-const thoughtSchema = new Schema({
-  thoughtText: {
-    type: String,
-    required: true,
-    minlength: 1,
-    max_length: 280,
+const thoughtSchema = new Schema(
+  {
+    thoughtText: {
+      type: String,
+      required: true,
+      minlength: 1,
+      max_length: 280,
+    },
+    createdAt: {
+      type: Date,
+      default: Date.now,
+      get: (time) => new Date(time).toLocaleDateString(),
+    },
+    username: {
+      type: String,
+      required: true,
+    },
+    //* Nested Document
+    reactions: [reactionSchema],
   },
-  createdAt: {
-    type: Date,
-    default: Date.now,
-    get: (time) => new Date(time).toLocaleDateString(),
-  },
-  username: {
-    type: String,
-    required: true,
-  },
-  //* Nested Document
-  reactions: [reactionSchema],
-  toJSON: {
-    virtuals: true,
-    getters: true,
-  },
-});
+  {
+    toJSON: {
+      virtuals: true,
+      getters: true,
+    },
+  }
+);
 
 thoughtSchema.virtual("reactionCount").get(function () {
   return this.reactions.length;
--- models/index.js
+++ models/index.js
@@ -1,5 +1,6 @@
 const Thought = require("./Thought")
 const User = require("./User")
+const Reaction = require("./Reaction")
 
 module.exports = { Thought, Reaction, User }
 
